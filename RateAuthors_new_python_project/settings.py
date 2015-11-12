483f9cd04259aafda426a5d3ceab30dab2fe7509
Haoran <haleyyew@gmail.com>
2015-11-11 18:38:42 -0800

    Adjust template path for deployment

--- RateAuthors_new_python_project/settings.py
+++ RateAuthors_new_python_project/settings.py
@@ -101,29 +101,24 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# TEMPLATE_PATH = os.path.join(BASE_DIR, 'RateAuthors_new_python_project/../templates')
+
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.core.context_processors.request',
+#                 'django.contrib.messages.context_processors.messages',
 #
-# TEMPLATE_DIRS = (
-#     TEMPLATE_PATH,
-# )
+#             ],
+#         },
+#     },
+# ]
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.request',
-                'django.contrib.messages.context_processors.messages',
-
-            ],
-        },
-    },
-]
-
-# TEMPLATE_DIRS = ('/home/haleyyew/personal_proj3/templates',
-#                  )
+TEMPLATE_DIRS = ('/home/haleyyew/personal_proj3/templates',)
