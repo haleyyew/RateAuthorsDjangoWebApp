32a5ff3d012265d7cf4dc24a7354cf150cd71b4b
Haoran <haleyyew@gmail.com>
2015-11-11 18:37:03 -0800

    Added CSS styling

--- RateAuthorsMainApp/views.py
+++ RateAuthorsMainApp/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse("Hey There!")
-    context_dict = {'boldmessage': "Please find a blog in our list that you want to rate."}
+    context_dict = {'boldmessage': "Please first click \"Load Data\" to load all tracks on SoundCloud to the table, then click on a track in the table below to play the track."}
     return render(request, 'RateAuthorsMainApp/index.html', context_dict)
 
