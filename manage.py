a2da7821cf51425028fbba75365220f103f5c00b
Haoran <haleyyew@gmail.com>
2015-12-06 15:48:23 -0800

    Fix minor errors

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 #
 # workflow on deployment:
 # workon rater
-# mv personal_proj3 personal_proj3.9.4
+# mv personal_proj3 personal_proj3.9.5
 # git clone https://haleyyew:HHYYbbyy920617_@github.com/haleyyew/personal_proj3.git
 # cd personal_proj3
 # python manage.py makemigrations BlogRater
